9fb070674efe8fc18fdc681ad2ad580ca240cd27
Lightfire228 <lightfire228@gmail.com>
2020-08-26 03:02:46 -0400

    switched to pulling down the pdf and using black magic image extraction code

--- cryptoquip/app.py
+++ cryptoquip/app.py
@@ -1,15 +1,20 @@
 
 from . import export_word
+from . import extract
 from . import menu
 from . import image
 from . import request
+
+from . import utils
 
 def run():
     image_contexts   = request.get_image_contexts()
     selected_context = menu.choose_image(image_contexts)
 
-    image_binary = request.download_image_binary(selected_context)
-    image_file   = image.process_image(image_binary, selected_context)
+    pdf_binary   = request.download_pdf_binary(selected_context)
+    extract.extract_image(pdf_binary, selected_context)
+    # image_binary = extract.extract_image(pdf_binary, selected_context)
+    # image_binary = request.download_image_binary(selected_context)
+    # image_file   = image.process_image(image_binary, selected_context)
 
-
-    image_file.close()
+    # image_file.close()
--- cryptoquip/utils.py
+++ cryptoquip/utils.py
@@ -3,22 +3,28 @@ from pathlib import Path
 
 from bs4 import BeautifulSoup
 
-def log_bs4(data):
+def log_bs4(data, filename='test', wrap=False):
 
-    from collections.abc import Iterable
-    
-    out = BeautifulSoup('<html><body></body></html>', 'parser.html')
+    file_ = Path('./out/') / f'{filename}.html' 
 
-    if not isinstance(data, Iterable):
+    out = BeautifulSoup('<html><body></body></html>', 'html.parser')
+
+    if wrap:
         data = [data]
 
     out.html.body.extend(data)
 
-    Path('./out/test.html').write_text(out.prettify())
+    file_.write_text(out.prettify())
 
 
-def log_img(image):
+def log_img(image, filename='test'):
 
-    file = Path('./out/test.png')
+    file_ = Path('./out/') / f'{filename}.png'
 
-    image.save(str(file), 'PNG')
+    image.save(str(file_), 'PNG')
+
+def log_pdf(pdf_binary, filename='test'):
+
+    file_ = Path('./out/') / f'{filename}.pdf'
+
+    file_.write_bytes(pdf_binary)
