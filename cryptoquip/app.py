d230fe1504209d0453c7402b997dc393ab2c3e28
Lightfire228 <lightfire228@gmail.com>
2020-08-25 23:49:39 -0400

    refacored process image module

--- cryptoquip/app.py
+++ cryptoquip/app.py
@@ -5,6 +5,11 @@ from . import process_image
 from . import request
 
 def run():
-    image_contexts    = request.get_image_contexts()
-    # selected_contexts = menu.
-    menu.choose_image(image_contexts)
+    image_contexts   = request.get_image_contexts()
+    selected_context = menu.choose_image(image_contexts)
+
+    image_binary = request.download_image_binary(selected_context)
+    image_file   = process_image.process_image(image_binary, selected_context)
+
+
+    image_file.close()
