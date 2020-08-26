08a843fcfb6cc5cdb3479ad24eb3f1df6d7089c7
Lightfire228 <lightfire228@gmail.com>
2020-08-26 03:39:08 -0400

    changed over image module to use pdf image

--- cryptoquip/app.py
+++ cryptoquip/app.py
@@ -12,9 +12,10 @@ def run():
     selected_context = menu.choose_image(image_contexts)
 
     pdf_binary   = request.download_pdf_binary(selected_context)
-    extract.extract_image(pdf_binary, selected_context)
-    # image_binary = extract.extract_image(pdf_binary, selected_context)
-    # image_binary = request.download_image_binary(selected_context)
+    image_binary = extract.extract_image(pdf_binary, selected_context)
+    image_       = image.process_image(image_binary, selected_context)
     # image_file   = image.process_image(image_binary, selected_context)
+
+    utils.log_img(image_)
 
     # image_file.close()
